441d0d9573904a2475a2b558addffc32550701fb
Bishwash Khanal <lionelbishwash11@gmail.com>
2023-02-11 18:46:21 +0545

    improved flow of code

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from utils import load_data
 from simple_L_layer_NN import L_layer_model, predict
 
-
 train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
-m_train = train_x_orig.shape[0]
-num_px = train_x_orig.shape[1]
+m_train, num_px  = train_x_orig.shape[:2]
 m_test = test_x_orig.shape[0]
 
 # Reshape the training and test examples 
@@ -14,7 +12,8 @@ test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
 # Standardize data to have feature values between 0 and 1.
 train_x = train_x_flatten/255.
 test_x = test_x_flatten/255.
-# CONSTANTS
+
+# number of hidden units in each layer
 layers_dims = [12288, 20, 7, 5, 1] #  4-layer model
 
 parameters, costs = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
